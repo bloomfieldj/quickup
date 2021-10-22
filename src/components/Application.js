f376d9ed72017efa744950aa23f03a75efb970d5
Jonathon Bloomfield <bloomfield.codes@gmail.com>
2021-10-22 14:57:02 -0400

    create navbar

---
+++ src/components/Application.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Application.scss";
+export default function Application() {
+  return (
+
+    <main className="layout">
+      <nav id="navbar" className="navbar">
+        <section id="navbar_side">
+          <span> Home </span>
+          <span> How It Works </span>
+        </section>
+        <span id="title"><strong>QuickUp</strong></span>
+        <section id="navbar_side">
+          <span> Sign-Up</span>
+          <span> Login </span>
+        </section>
+      </nav>
+    </main>
+  );
+}
