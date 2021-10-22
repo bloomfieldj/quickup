35acc8eea579f6d7ed9526dbe946ffade9cbfd23
Jonathon Bloomfield <bloomfield.codes@gmail.com>
2021-10-22 16:01:59 -0400

    add bio to profile creation

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -23,7 +23,10 @@ export default function Profile() {
           <input type="tel" class='profile_entry' name="phone" placeholder="Phone Number" />
           <input type="email" class='profile_entry' name="email" placeholder="Email" />
           <input type="text" class='profile_entry' name="job" placeholder="Occupation" />
-
+        </section>
+        <section>
+          <textarea name="bio" class="profile_entry" rows="6" cols="50" placeholder="I'm just a a Pam looking for my Jim. My hobbies include Netflix, UberEats and drowning my existential dread in Nutella. Looking for someone who will treat me like the royalty I am. Must be 6'2 or taller and make 150k+ or don't even bother.">
+          </textarea>
         </section>
       </form>
     </Fragment>
