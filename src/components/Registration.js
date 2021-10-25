56bfb02b0623792d02ef00327d6ee912057b7226
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-10-25 17:20:54 -0400

    add styling on welcome page

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,8 +1,6 @@
 import React, { Fragment } from "react";
 import "./Registration.scss";
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
 export default function Registration() {
 
   return (
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,21 @@
 import React, { Fragment, useState } from "react";
-import Registration from "./Registration";
+import "./Welcome.scss"
+
 
 export default function Welcome(props){
 
   return(
     <Fragment> 
     <h1>Welcome to Quickup!</h1>
-    <p>To start chatting, simply log in or register!</p>
-    <button onClick={props.onClick1}> Log In </button>
-    <button onClick={props.onClick2}> Register </button>
+    <div className="welcome">
+      <div>
+      <p className="greeting">To start chatting, simply log in or register!</p>
+      </div>
+      <div className="call-to-action">
+        <button className="login" onClick={props.onClick1}> Log In </button>
+        <button className="register" onClick={props.onClick2}> Register </button>
+      </div>
+    </div>
     </Fragment>
   )
 }
