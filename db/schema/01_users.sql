f8107391ef61ad13363eb4c34c13024bd8a5ecd8
Rebekah <rebekahmparkes@outlook.com>
2021-10-26 09:10:37 -0400

    navbar and borders styling

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,17 +1,17 @@
-
 DROP TABLE IF EXISTS users CASCADE;
 
 -- Create Users table
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
   age SMALLINT NOT NULL,
-  gender VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  -- password VARCHAR(255) NOT NULL,
+  gender VARCHAR(15) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   phone VARCHAR(20) NOT NULL
-  city VARCHAR(255) NOT NULL, 
+  city VARCHAR(50) NOT NULL, 
   occupation VARCHAR(255) NOT NULL, 
+  bio VARCHAR(1000) NOT NULL
 );
