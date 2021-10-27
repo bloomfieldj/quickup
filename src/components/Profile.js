1c074aae561b031d18b97597e208b39ff7bc1e50
Rebekah <rebekahmparkes@outlook.com>
2021-10-27 12:48:49 -0400

    changes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,19 +7,20 @@ export default function Profile(props) {
     <Fragment>
       <h1> My profile</h1>
       <section id='profile'>
-
-        <img id="profile_pic" src={props.profile.photo} alt="profile-pic" width="100" height="100" />
-        
-  
+        <div class="profile-body">
+          <div class="photo">
+            <img id="profile_pic" src={props.profile.photo} alt="profile-pic" width="100" height="100" />
+          </div>
+        </div>
         <section id="details">
           <section className='profile-section1'>
             <div>
-            <h2>{props.profile.first_name}</h2>
-            <h2>{props.profile.last_name}</h2>
+              <h2>{props.profile.first_name}</h2>
+              <h2>{props.profile.last_name}</h2>
             </div>
             <div>
-            <h2>{props.profile.age}</h2>
-            <h2>{props.profile.gender}</h2>
+              <h2>{props.profile.age}</h2>
+              <h2>{props.profile.gender}</h2>
             </div>
           </section>
           <section className='profile-section2'>
@@ -31,8 +32,11 @@ export default function Profile(props) {
         </section>
       </section>
       <p id="bio">
-          {props.profile.bio}
-        </p>
+        {props.profile.bio}
+      </p>
+
+
+
     </Fragment>
   );
 }
