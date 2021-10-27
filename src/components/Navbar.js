4604f1fd482c0e8bedb6b0dfa5ae002819bf6b01
Rebekah <rebekahmparkes@outlook.com>
2021-10-27 09:35:48 -0400

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,25 @@
 
-export default function Navbar(props){
+export default function Navbar(props) {
   return (
     <nav id="navbar" className="navbar">
-        <section id="navbar_side">
-          <span> <button> How It Works </button> </span>
-          <span> <button onClick={props.onClick3}>Your Profile</button> </span>
-          <span> <button onClick={props.onClick4}>Visit Chat Room</button> </span>
-        </section>
-        <span id="title"><strong>QuickUp</strong></span>
-        <section id="navbar_side">
-          <span> <button onClick={props.onClick2}>Sign-Up</button></span>
-          <span> <button onClick={props.onClick1}>Login </button></span>
-        </section>
-      </nav>
+      <section id="navbar_side">
+      <section class="dropdown">
+     
+        <button class="dropbtn"> <span id="title"><strong>QuickUp</strong></span>
+        <i class ="fa fa-caret-down"></i>
+        </button>
+        <div class ="dropdown-content">
+        <button> How It Works </button>
+        <button onClick={props.onClick3}>Your Profile</button> 
+        <button onClick={props.onClick4}>Visit Chat Room</button>
+        </div>
+      </section>
+
+      </section>
+      <section id="navbar_side">
+        <span> <button onClick={props.onClick2}>Sign-Up</button></span>
+        <span> <button onClick={props.onClick1}>Login </button></span>
+      </section>
+    </nav>
   );
 }
