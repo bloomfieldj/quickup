95e13d214c84cd50b2820205163175978b1a12cb
Rebekah <rebekahmparkes@outlook.com>
2021-10-27 10:32:03 -0400

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faFolder, faFolderOpen } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar(props) {
   return (
     <nav id="navbar" className="navbar">
       <section id="navbar_side">
-      <section class="dropdown">
-     
-        <button class="dropbtn"> <span id="title"><strong>QuickUp</strong></span>
-        <i class ="fa fa-caret-down"></i>
-        </button>
-        <div class ="dropdown-content">
-        <button> How It Works </button>
-        <button onClick={props.onClick3}>Your Profile</button> 
-        <button onClick={props.onClick4}>Visit Chat Room</button>
-        </div>
-      </section>
+
+        <section class="dropdown">
+          <button class="dropbtn">
+            <span id="title"><strong>QuickUp</strong></span><div class="dropcarat"><FontAwesomeIcon icon={faChevronDown} /></div>
+          </button>
+          <div class="dropdown-content">
+           <button> How It Works </button>
+            <button onClick={props.onClick3}>Your Profile</button>
+            <button onClick={props.onClick4}>Visit Chat Room</button>
+          </div>
+        </section>
 
       </section>
       <section id="navbar_side">
