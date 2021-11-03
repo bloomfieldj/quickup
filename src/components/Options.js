6093c806d9fc660edc5342c33075aa64b378c08a
Rebekah <rebekahmparkes@outlook.com>
2021-11-03 06:47:37 -0400

    button fix

--- src/components/Options.js
+++ src/components/Options.js
@@ -82,7 +82,7 @@ function handleClick(event){
           
         <div class="enter-room-cont">
             <button class="start-chatting" onClick={(event) => handleClick(event)}> Start Chatting with Mystery Person #1!</button>
-            <button onClick={(event) => handleClick2(event)}> Start Chatting with Mystery Person #2!</button>
+            <button class="start-chatting" onClick={(event) => handleClick2(event)}> Start Chatting with Mystery Person #2!</button>
         </div>
           </>
         )}
